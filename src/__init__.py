a1a375bd439e20951b7274e2c410bf07eab049ed
damoncrockett <damoncrockett@hotmail.com>
2018-05-14 16:58:38 -0400

    folder mv
    Signed-off-by: damoncrockett <damoncrockett@hotmail.com>

--- src/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-__author__ = 'Damon Crockett'
-__email__ = 'damoncrockett@gmail.com'
-__version__ = '0.0.1'
-
-from data import attach,detach
-from plot import show,montage,histogram,scatter,compose
-from extract import extract
