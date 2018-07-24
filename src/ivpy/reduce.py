081deaaf5d7beea62d5ed8fe15acccea3e6e3d5a
damoncrockett <damoncrockett@hotmail.com>
2018-07-24 17:05:59 -0400

    reduce enforce pd
    Signed-off-by: damoncrockett <damoncrockett@hotmail.com>

--- src/ivpy/reduce.py
+++ src/ivpy/reduce.py
@@ -5,9 +5,15 @@ import umap as ump
 #------------------------------------------------------------------------------
 
 def tsne(X,**kwargs):
+    if not isinstance(X,pd.DataFrame):
+        raise TypeError("Feature matrix must be a pandas DataFrame")
+
     xy = TSNE(n_components=2,**kwargs).fit_transform(X)
-    return pd.DataFrame(xy,columns=['x','y'])
+    return pd.DataFrame(xy,index=X.index,columns=['x','y'])
 
 def umap(X,**kwargs):
+    if not isinstance(X,pd.DataFrame):
+        raise TypeError("Feature matrix must be a pandas DataFrame")
+
     xy = ump.UMAP(**kwargs).fit_transform(X)
-    return pd.DataFrame(xy,columns=['x','y'])
+    return pd.DataFrame(xy,index=X.index,columns=['x','y'])
