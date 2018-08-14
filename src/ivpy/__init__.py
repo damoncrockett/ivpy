3205ab25bf3077d7c08e887cb7e310c9b7146729
damoncrockett <damoncrockett@hotmail.com>
2018-08-14 00:01:54 -0400

    init chg
    Signed-off-by: damoncrockett <damoncrockett@hotmail.com>

--- src/ivpy/__init__.py
+++ src/ivpy/__init__.py
@@ -4,5 +4,6 @@ __author__ = 'Damon Crockett'
 __email__ = 'damoncrockett@gmail.com'
 __version__ = '0.0.1'
 
-from ivpy.data import attach,detach,resize
+from ivpy.data import attach,detach
 from ivpy.plot import show,montage,histogram,scatter,compose
+from ivpy.utils import resize
