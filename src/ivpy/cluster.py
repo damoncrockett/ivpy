5567a7259638596ba61022aeca0c35df3c52cbe3
damoncrockett <damoncrockett@hotmail.com>
2019-03-14 17:44:17 -0400

    set centroids in kmeans
    Signed-off-by: damoncrockett <damoncrockett@hotmail.com>

--- src/ivpy/cluster.py
+++ src/ivpy/cluster.py
@@ -5,24 +5,34 @@ from .data import _typecheck
 #------------------------------------------------------------------------------
 
 """
-Note: it's possible that if the user passes an X whose index doesn't match 
+Note: it's possible that if the user passes an X whose index doesn't match
 the pathcol they'll ultimately use to plot, you could have an indexing issue.
 Not really any way to prevent that, but it's unlikely to happen because X will
 often be obtained using extract(), which guards against it.
 """
 
-def cluster(X,method=None,k=None,**kwargs):
-    if method is None:
-        raise ValueError("Must supply 'method'")
-    if k is None:
-        raise ValueError("Must supply number of clusters 'k'")
-
+def cluster(X,method='kmeans',k=4,centroids=None,**kwargs):
     _typecheck(**locals())
 
     if method=='kmeans':
-        return _cluster(X,KMeans,n_clusters=k,**kwargs)
+        if centroids is not None:
+            k = len(centroids)
+            print("method:",method,"\nnumber of clusters:",str(k))
+            return _cluster(X,
+                            KMeans,
+                            n_clusters=k,
+                            init=X.loc[centroids],
+                            **kwargs)
+        elif centroids is None:
+            print("method:",method,"\nnumber of clusters:",str(k))
+            return _cluster(X,KMeans,n_clusters=k,**kwargs)
+
     elif method=='hierarchical':
-        return _cluster(X,AgglomerativeClustering,n_clusters=k,**kwargs)
+        print("method:",method,"\nnumber of clusters:",str(k))
+        return _cluster(X,
+                        AgglomerativeClustering,
+                        n_clusters=k,
+                        **kwargs)
 
 def _cluster(X,func,**kwargs):
     fitted = func(**kwargs).fit(X)
