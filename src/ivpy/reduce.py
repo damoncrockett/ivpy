deaaeb44fc35dffaf536d34ba029c15d0ca00d69
damoncrockett <damoncrockett@hotmail.com>
2019-04-17 15:51:52 -0400

    PCA to reduce
    Signed-off-by: damoncrockett <damoncrockett@hotmail.com>

--- src/ivpy/reduce.py
+++ src/ivpy/reduce.py
@@ -1,9 +1,15 @@
 import pandas as pd
 from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
 import umap as ump
 from .data import _typecheck
 
 #------------------------------------------------------------------------------
+
+def pca(X,**kwargs):
+    _typecheck(**locals())
+    xy = PCA(n_components=2,**kwargs).fit_transform(X)
+    return pd.DataFrame(xy,index=X.index,columns=['x','y'])
 
 def tsne(X,**kwargs):
     _typecheck(**locals())
